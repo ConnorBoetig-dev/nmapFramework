6eb9b7b958ba72ae01f4ed4ccec88bef248d5c15
ConnorBoetig-dev <connorboetig20@gmail.com>
2025-06-06 21:21:18 -0400

    cleaner

---
+++ helpers/clean_output.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm -f output/processed/* \
+      output/topology/* \
+      output/reports/csv/* \
+      output/reports/diffs/* \
+      output/reports/html/* \
+      output/xml/processed/* \
+      output/xml/processed/raw/* \
+      output/xml/raw/*
+echo "🧹 Scan output cleaned."
